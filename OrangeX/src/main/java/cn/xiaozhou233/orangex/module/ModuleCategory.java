ca66b29992bcebcf8f5232ee4cb02f959a37e433
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-16 15:46:09 +0800

    [Feat] ModuleManager [Feat] ClickGUI Base [Feat] HUD now display modules

---
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/module/ModuleCategory.java
@@ -0,0 +1,5 @@
+package cn.xiaozhou233.orangex.module;
+
+public enum ModuleCategory {
+    RENDER, MISC
+}
--- OrangeX/src/main/java/cn/xiaozhou233/orangex/ui/clickgui/ClickGuiScreen.java
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/ui/clickgui/ClickGuiScreen.java
@@ -1,14 +1,29 @@
 package cn.xiaozhou233.orangex.ui.clickgui;
 
+import cn.xiaozhou233.orangex.ui.clickgui.component.PanelUI;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class ClickGuiScreen extends GuiScreen {
+    private PanelUI panel;
+
+    @Override
+    public void initGui() {
+        panel = new PanelUI(10, 10, 100, 150, "Example");
+    }
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        drawDefaultBackground();
+        // Background
+        int color = new Color(50, 50, 50, 100).getRGB();
+        Gui.drawRect(0, 0, this.width, this.height, color);
+
+        if (panel != null) {
+            panel.draw();
+        }
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
