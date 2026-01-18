5beb4a25f3dd011f8b849405ae7de272d743240b
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-18 19:58:52 +0800

    [Feat] Enable color

--- OrangeX/src/main/java/cn/xiaozhou233/orangex/ui/clickgui/render/GuiRenderUtils.java
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/ui/clickgui/render/GuiRenderUtils.java
@@ -32,4 +32,27 @@ public class GuiRenderUtils {
     public static void disableBlend() {
         GL11.glDisable(GL11.GL_BLEND);
     }
+
+    public static int blendColor(int color1, int color2, float ratio) {
+        if (ratio <= 0f) return color1;
+        if (ratio >= 1f) return color2;
+
+        int a1 = (color1 >> 24) & 0xFF;
+        int r1 = (color1 >> 16) & 0xFF;
+        int g1 = (color1 >> 8) & 0xFF;
+        int b1 = color1 & 0xFF;
+
+        int a2 = (color2 >> 24) & 0xFF;
+        int r2 = (color2 >> 16) & 0xFF;
+        int g2 = (color2 >> 8) & 0xFF;
+        int b2 = color2 & 0xFF;
+
+        int a = (int) (a1 + (a2 - a1) * ratio);
+        int r = (int) (r1 + (r2 - r1) * ratio);
+        int g = (int) (g1 + (g2 - g1) * ratio);
+        int b = (int) (b1 + (b2 - b1) * ratio);
+
+        return (a << 24) | (r << 16) | (g << 8) | b;
+    }
+
 }
