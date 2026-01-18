2f7fb513fa30a1b0a8d369d77e8142187d6b6607
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-18 18:39:51 +0800

    [Feat] ClickGUI Font

--- OrangeX/src/main/java/cn/xiaozhou233/orangex/font/FontManager.java
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/font/FontManager.java
@@ -3,10 +3,14 @@ package cn.xiaozhou233.orangex.font;
 import java.awt.*;
 
 public class FontManager {
+    public FontRenderer MontserratLight18 = new FontRenderer("Montserrat-Light.ttf", 18, Font.PLAIN, true, true);
+
     public FontRenderer MontserratRegular18 = new FontRenderer("Montserrat-Bold.ttf", 18, Font.PLAIN, true, true);
     public FontRenderer MontserratRegular32 = new FontRenderer("Montserrat-Bold.ttf", 32, Font.PLAIN, true, true);
+
     public FontRenderer MontserratMedium18 = new FontRenderer("Montserrat-Medium.ttf", 18, Font.PLAIN, true, true);
     public FontRenderer MontserratMedium32 = new FontRenderer("Montserrat-Medium.ttf", 32, Font.PLAIN, true, true);
+
     public FontRenderer MontserratBold18 = new FontRenderer("Montserrat-Bold.ttf", 18, Font.PLAIN, true, true);
     public FontRenderer MontserratBold32 = new FontRenderer("Montserrat-Bold.ttf", 32, Font.PLAIN, true, true);
 
