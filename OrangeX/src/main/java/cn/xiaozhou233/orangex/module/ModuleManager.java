828e6bc8c2692754550e89635ea00e05f43b2156
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-20 16:07:50 +0800

    [Delete] AutoClicker

--- OrangeX/src/main/java/cn/xiaozhou233/orangex/module/ModuleManager.java
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/module/ModuleManager.java
@@ -2,7 +2,6 @@ package cn.xiaozhou233.orangex.module;
 
 import cn.xiaozhou233.orangex.OrangeX;
 import cn.xiaozhou233.orangex.event.impl.EventKey;
-import cn.xiaozhou233.orangex.module.impl.combat.AutoClicker;
 import cn.xiaozhou233.orangex.module.impl.misc.Test;
 import cn.xiaozhou233.orangex.module.impl.render.*;
 import org.greenrobot.eventbus.Subscribe;
@@ -29,7 +28,6 @@ public class ModuleManager {
 
         register(new Test());
 
-        register(new AutoClicker());
     }
 
     @Subscribe
