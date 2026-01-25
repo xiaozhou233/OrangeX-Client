3d1cf7f27bc53f050f3add92f545581d959d504f
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-25 14:33:06 +0800

    [Feat] NoJumpDelay

--- OrangeX/src/main/java/cn/xiaozhou233/orangex/module/ModuleManager.java
+++ OrangeX/src/main/java/cn/xiaozhou233/orangex/module/ModuleManager.java
@@ -4,6 +4,7 @@ import cn.xiaozhou233.orangex.OrangeX;
 import cn.xiaozhou233.orangex.event.impl.EventKey;
 import cn.xiaozhou233.orangex.module.impl.fun.Derp;
 import cn.xiaozhou233.orangex.module.impl.misc.Test;
+import cn.xiaozhou233.orangex.module.impl.movement.NoJumpDelay;
 import cn.xiaozhou233.orangex.module.impl.render.*;
 import org.greenrobot.eventbus.Subscribe;
 
@@ -31,6 +32,8 @@ public class ModuleManager {
         register(new Test());
 
         register(new Derp());
+
+        register(new NoJumpDelay());
     }
 
     @Subscribe
